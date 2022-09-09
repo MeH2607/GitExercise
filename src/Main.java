13ca1c64d30c6c5991f4e56558482bf9dcd9c23e
MeH2607 <moha898h@stud.kea.dk>
2022-09-09 09:57:27 +0200

    Added greet Amend: rykkede lige Main en gang opad

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
+
 public class Main {
     public static void main(String[] args) {
 
 
         SuperGreeter superGreeter = new SuperGreeter();
         superGreeter.start();
+        superGreeter.greet();
 
     }
 }
--- src/SuperGreeter.java
+++ src/SuperGreeter.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class SuperGreeter {
+
+    Scanner sc;
     public SuperGreeter(){
+        sc = new Scanner(System.in);
     }
 
     public void start(){
         System.out.println("DET VIRKER YOOOOO!!!");
+    }
+
+    public void greet (){
+        String navn;
+        System.out.println("hvad hedder du: ");
+        navn = sc.nextLine();
+        System.out.println("Hej " + navn);
     }
 }
